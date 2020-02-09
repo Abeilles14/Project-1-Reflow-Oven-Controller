83942b727ea794ba0a86914754c68638579767fc
Deboru <56138820+Deboru@users.noreply.github.com>
2020-02-09 14:04:29 -0800

    Update FSM_Macro.asm

--- FSM_Macro.asm
+++ FSM_Macro.asm
@@ -1,43 +1,29 @@
-state_done: dbit 1
-
-Timer_done: dbit 1
-goal_temp: ds 2 ;desired period for current state
-current_state_time: ds 1 ;time elapsed for current state
-current_temp: ds 2
-
-
-
-;mov goal_temp, 100
-
-FSMstate(goal_temp, goal_time, current_state_time) ;example macro call
-
-
+;----------------------------;
+;FSM Macro combining logics  ;
+;calling example:	     ;
+;FSMstate(goaltemp, goaltime);
+;inputs are of decimal values;
+;----------------------------;
 FSMstate mac
 
 skip%Mb:
 
 	;time check
-	mov a, goal_time
+	mov a, %1
 	clr c
-	subb a, current_state_time
+	subb a, BCD_counterSec
 	jb c, skip%Ma			;if goal_time < current_state_time, end the state
 
 	;temp check
+	lcall Read_ADC_Channel
 	lcall GetTemp
-	; resulting current temp saved in current_temp+1 to current_temp+0
+	; resulting current temp saved in currentTemp (decimal value)
 	
-	;compare higher byte temperature values
-	mov a, %0 +1
-	clr c
-	subb a, current_temp+1
-	jb c, skip%Ma 	;c = 1 if goal_temp-high < current_temp-high, end the state
-	cjne a, #0x00, skip%Mb	;if gial_temp-high = current_temp-high, check current_temp-low
-					
 	;compare lower byte temperature values
 	mov a, %0
 	clr c
-	subb a, current_temp
-	jb c, skip%Ma	;c = 1 if goal_temp-low < current_temp-low, end the state
+	subb a, currentTemp
+	jb c, skip%Ma	;c = 1 if goal_temp < current_temp, end the state
 	ljmp skip%Mb
 	
 skip%Ma:
