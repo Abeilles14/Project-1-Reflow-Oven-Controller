49bcff8e4a483ce61ec23b2212ce5f4ea6d016a1
Abeilles14 <40010108+Abeilles14@users.noreply.github.com>
2020-02-10 13:34:15 -0800

    temp stuff

---
+++ updated_temp_test/TempTesting.asm
@@ -0,0 +1,312 @@
+$MODLP51
+org 0000H
+ljmp MainProgram
+
+ ;DECLARATIONS
+CLK         EQU 14746000  ; Microcontroller system clock frequency in Hz
+CCU_RATE    EQU 22050     ; 22050Hz is the sampling rate of the wav file we are playing
+CCU_RELOAD  EQU ((65536-((CLK/(2*CCU_RATE)))))
+BAUD        EQU 115200
+BRVAL       EQU ((CLK/BAUD)-16)
+
+FLASH_CE EQU P2.4
+MY_MOSI EQU P2.2
+MY_MISO EQU P2.3
+MY_SCLK EQU P2.5
+
+; Commands supported by the SPI flash memory according to the datasheet
+WRITE_ENABLE     EQU 0x06  ; Address:0 Dummy:0 Num:0
+WRITE_DISABLE    EQU 0x04  ; Address:0 Dummy:0 Num:0
+READ_STATUS      EQU 0x05  ; Address:0 Dummy:0 Num:1 to infinite
+READ_BYTES       EQU 0x03  ; Address:3 Dummy:0 Num:1 to infinite
+READ_SILICON_ID  EQU 0xab  ; Address:0 Dummy:3 Num:1 to infinite
+FAST_READ        EQU 0x0b  ; Address:3 Dummy:1 Num:1 to infinite
+WRITE_STATUS     EQU 0x01  ; Address:0 Dummy:0 Num:1
+WRITE_BYTES      EQU 0x02  ; Address:3 Dummy:0 Num:1 to 256
+ERASE_ALL        EQU 0xc7  ; Address:0 Dummy:0 Num:0
+ERASE_BLOCK      EQU 0xd8  ; Address:3 Dummy:0 Num:0
+READ_DEVICE_ID   EQU 0x9f  ; Address:0 Dummy:2 Num:1 to infinite
+
+CSEG
+; These 'equ' must match the wiring between the microcontroller and the LCD!
+LCD_RS equ P0.5
+LCD_RW equ P0.6
+LCD_E  equ P0.7
+LCD_D4 equ P1.2
+LCD_D5 equ P1.3
+LCD_D6 equ P1.4
+LCD_D7 equ P1.6
+
+$NOLIST
+$include(LCD_4bit.inc) ; A library of LCD related functions and utility macros
+$include(math32.inc)
+$include(will.inc)
+$include(tempcheck.inc)
+$include(LCD_4bit_LPC9351.inc)
+
+DSEG at 0x30
+buffer: ds 30
+x:   ds 4
+y:   ds 4
+bcd: ds 5
+
+LM_Result: ds 2
+TC_Result: ds 1
+Result: ds 2
+temp:ds 2
+LM_TEMP: ds 2
+
+bseg
+mf: dbit 1
+
+
+;---------------------------------;
+; Sends a byte via serial port    ;
+;---------------------------------;
+putchar:
+	jbc	TI,putchar_L1
+	sjmp putchar
+putchar_L1:
+	mov	SBUF,a
+	ret
+
+;---------------------------------;
+; Receive a byte from serial port ;
+;---------------------------------;
+getchar:
+	jbc	RI,getchar_L1
+	sjmp getchar
+getchar_L1:
+	mov	a,SBUF
+	ret
+
+;---------------------------------;
+; Initialize the serial port      ;
+;---------------------------------;
+InitSerialPort:
+	mov	BRGCON,#0x00
+	mov	BRGR1,#high(BRVAL)
+	mov	BRGR0,#low(BRVAL)
+	mov	BRGCON,#0x03 ; Turn-on the baud rate generator
+	mov	SCON,#0x52 ; Serial port in mode 1, ren, txrdy, rxempty
+	; Make sure that TXD(P1.0) and RXD(P1.1) are configured as bidrectional I/O
+	anl	P1M1,#11111100B
+	anl	P1M2,#11111100B
+	ret
+
+
+;---------------------------------;
+; Initialize ADC1/DAC1 as DAC1.   ;
+; Warning, the ADC1/DAC1 can work ;
+; only as ADC or DAC, not both.   ;
+; The P89LPC9351 has two ADC/DAC  ;
+; interfaces.  One can be used as ;
+; ADC and the other can be used   ;
+; as DAC.  Also configures the    ;
+; pin associated with the DAC, in ;
+; this case P0.4 as 'Open Drain'. ;
+;---------------------------------;
+InitDAC1:
+    ; Configure pin P0.4 (DAC1 output pin) as open drain
+	orl	P0M1,   #00010000B
+	orl	P0M2,   #00010000B
+    mov ADMODB, #00101000B ; Select main clock/2 for ADC/DAC.  Also enable DAC1 output (Table 25 of reference manual)
+	mov	ADCON1, #00000100B ; Enable the converter
+	mov AD1DAT3, #0x80     ; Start value is 3.3V/2 (zero reference for AC WAV file)
+	ret
+
+;---------------------------------;
+; Initialize ADC0/DAC0 as ADC0.   ;
+;---------------------------------;
+InitADC0:
+	; ADC0_0 is connected to P1.7
+	; ADC0_1 is connected to P0.0
+	; ADC0_2 is connected to P2.1
+	; ADC0_3 is connected to P2.0
+    ; Configure pins P1.7, P0.0, P2.1, and P2.0 as inputs
+    orl P0M1, #00000001b
+    anl P0M2, #11111110b
+    orl P1M1, #10000000b
+    anl P1M2, #01111111b
+    orl P2M1, #00000011b
+    anl P2M2, #11111100b
+	; Setup ADC0
+	setb BURST0 ; Autoscan continuos conversion mode
+	mov	ADMODB,#0x20 ;ADC0 clock is 7.3728MHz/2
+	mov	ADINS,#0x0f ; Select the four channels of ADC0 for conversion
+	mov	ADCON0,#0x05 ; Enable the converter and start immediately
+	; Wait for first conversion to complete
+InitADC0_L1:
+	mov	a,ADCON0
+	jnb	acc.3,InitADC0_L1
+	ret
+
+;---------------------------------;
+; Change the internal RC osc. clk ;
+; from 7.373MHz to 14.746MHz.     ;
+;---------------------------------;
+Double_Clk:
+    mov dptr, #CLKCON
+    movx a, @dptr
+    orl a, #00001000B ; double the clock speed to 14.746MHz
+    movx @dptr,a
+	ret
+
+;---------------------------------;
+; Initialize the SPI interface    ;
+; and the pins associated to SPI. ;
+;---------------------------------;
+Init_SPI:
+	; Configure MOSI (P2.2), CS* (P2.4), and SPICLK (P2.5) as push-pull outputs (see table 42, page 51)
+	anl P2M1, #low(not(00110100B))
+	orl P2M2, #00110100B
+	; Configure MISO (P2.3) as input (see table 42, page 51)
+	orl P2M1, #00001000B
+	anl P2M2, #low(not(00001000B)) 
+	; Configure SPI
+	mov SPCTL, #11010000B ; Ignore /SS, Enable SPI, DORD=0, Master=1, CPOL=0, CPHA=0, clk/4
+	ret
+
+;---------------------------------;
+; Sends AND receives a byte via   ;
+; SPI.                            ;
+;---------------------------------;
+Send_SPI:
+	mov SPDAT, a
+Send_SPI_1:
+	mov a, SPSTAT 
+	jnb acc.7, Send_SPI_1 ; Check SPI Transfer Completion Flag
+	mov SPSTAT, a ; Clear SPI Transfer Completion Flag
+	mov a, SPDAT ; return received byte via accumulator
+	ret
+
+;---------------------------------;
+; SPI flash 'write enable'        ;
+; instruction.                    ;
+;---------------------------------;
+Enable_Write:
+	clr FLASH_CE
+	mov a, #WRITE_ENABLE
+	lcall Send_SPI
+	setb FLASH_CE
+	ret
+
+;---------------------------------;
+; This function checks the 'write ;
+; in progress' bit of the SPI     ;
+; flash memory.                   ;
+;---------------------------------;
+Check_WIP:
+	clr FLASH_CE
+	mov a, #READ_STATUS
+	lcall Send_SPI
+	mov a, #0x55
+	lcall Send_SPI
+	setb FLASH_CE
+	jb acc.0, Check_WIP ;  Check the Write in Progress bit
+	ret
+	
+;---------------------------------;
+; CRC-CCITT (XModem) Polynomial:  ;
+; x^16 + x^12 + x^5 + 1 (0x1021)  ;
+; CRC in [R7,R6].                 ;
+; Converted to a macro to remove  ;
+; the overhead of 'lcall' and     ;
+; 'ret' instructions, since this  ;
+; 'routine' may be executed over  ;
+; 4 million times!                ;
+;---------------------------------;
+;crc16:
+crc16 mac
+	xrl	a, r7			; XOR high of CRC with byte
+	mov r0, a			; Save for later use
+	mov	dptr, #CRC16_TH ; dptr points to table high
+	movc a, @a+dptr		; Get high part from table
+	xrl	a, r6			; XOR With low byte of CRC
+	mov	r7, a			; Store to high byte of CRC
+	mov a, r0			; Retrieve saved accumulator
+	mov	dptr, #CRC16_TL	; dptr points to table low	
+	movc a, @a+dptr		; Get Low from table
+	mov	r6, a			; Store to low byte of CRC
+	;ret
+endmac
+
+
+; Display a 3-digit BCD number in the LCD
+LCD_3BCD:
+	mov a, bcd+1
+	anl a, #0x0f
+	orl a, #'0'
+	lcall ?WriteData
+	mov a, bcd+0
+	swap a
+	anl a, #0x0f
+	orl a, #'0'
+	lcall ?WriteData
+	mov a, bcd+0
+	anl a, #0x0f
+	orl a, #'0'
+	lcall ?WriteData
+	ret
+	
+Display_ADC_Values:
+	; Analog input to pin P1.7
+	mov x+0, AD0DAT3
+	mov x+1, #0
+	mov x+2, #0
+	mov x+3, #0
+	lcall Hex2BCD
+	Set_Cursor(2, 1)
+	lcall LCD_3BCD
+	; Analog input to pin P0.0
+	mov x+0, AD0DAT2
+	mov x+1, #0
+	mov x+2, #0
+	mov x+3, #0
+	lcall Hex2BCD
+	Set_Cursor(2, 5)
+	lcall LCD_3BCD
+	; Analog input to pin P2.0
+	mov x+0, AD0DAT1
+	mov x+1, #0
+	mov x+2, #0
+	mov x+3, #0
+	lcall Hex2BCD
+	Set_Cursor(2, 9)
+	lcall LCD_3BCD
+	; Analog input to pin P2.1
+	mov x+0, AD0DAT0
+	mov x+1, #0
+	mov x+2, #0
+	mov x+3, #0
+	lcall Hex2BCD
+	Set_Cursor(2, 13)
+	lcall LCD_3BCD
+	; Some delay so the LCD looks ok
+	Wait_Milli_Seconds(#250)
+	ret
+
+
+MainProgram:
+
+    lcall Ports_Init ; Default all pins as bidirectional I/O. See Table 42.
+    lcall LCD_4BIT
+    lcall Double_Clk
+	lcall InitSerialPort
+	lcall InitADC0 ; Call after 'Ports_Init'
+	lcall InitDAC1 ; Call after 'Ports_Init'
+	lcall Init_SPI
+
+forever:
+	;lcall Display_ADC_Values
+
+	lcall ReadTemp
+	lcall Wait_Sec
+
+	Set_cursor (1,1) ;print to the lcd
+	Display_BCD(bcd+1)
+	Display_BCD(bcd+0)
+	WriteData(#'C')
+	sjmp forever
+
+END
